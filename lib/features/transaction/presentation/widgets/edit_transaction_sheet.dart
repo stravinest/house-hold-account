5d192efee964cba1c2a245080fb390f8aedb360d
stravinest <your-email@example.com>
2026-01-16 23:03:11 +0900

    auto-claude: subtask-3-1 - Import CategorySectionWidget and remove manual cat

--- lib/features/transaction/presentation/widgets/edit_transaction_sheet.dart
+++ lib/features/transaction/presentation/widgets/edit_transaction_sheet.dart
@@ -10,6 +10,7 @@ import '../../../payment_method/domain/entities/payment_method.dart';
 import '../../../payment_method/presentation/providers/payment_method_provider.dart';
 import '../../domain/entities/transaction.dart';
 import '../providers/transaction_provider.dart';
+import 'category_section_widget.dart';
 import 'transaction_form_fields.dart';
 
 /// 기존 거래를 수정하는 Bottom Sheet
